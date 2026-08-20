8f74c1e812ed8098d1f3624de470add9d0f07678
jenil-pixster <jenil.pixster@gmail.com>
2026-08-20 13:00:08 +0530

    Start Project
    - Created the swiftUI projects

---
+++ CurrencyConvertApp/CurrencyConvertApp/CurrencyConvertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertAppApp.swift
+//  CurrencyConvertApp
+//
+//  Created by Jenil - iOS Developer on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
